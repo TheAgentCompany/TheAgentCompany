102507125c3c1e1b320a37f8a92210acbfdf988c
Boxuan Li <liboxuan@connect.hku.hk>
2024-08-30 14:29:57 -0700

    Revert retry mechanism

--- servers/gitlab/custom_wrapper.sh
+++ servers/gitlab/custom_wrapper.sh
@@ -1,29 +1,10 @@
 #!/bin/bash
 set -ex
 
-MAX_RETRIES=3
-RETRY_DELAY=10
-
-function retry_command() {
-    local command="$1"
-    local max_attempts="$2"
-    local delay="$3"
-    local attempt=1
-
-    until eval "$command"
-    do
-        if ((attempt == max_attempts))
-        then
-            echo "Command failed after $max_attempts attempts. Exiting."
-            return 1
-        fi
-
-        echo "Attempt $attempt failed. Retrying in $delay seconds..."
-        sleep $delay
-        ((attempt++))
-    done
-    echo "Command succeeded"
-}
+# start from a clean state without any configs or data
+rm -rf /etc/gitlab/*
+rm -rf /var/log/gitlab/*
+rm -rf /var/opt/gitlab/*
 
 # Run the original wrapper script, but remove the last five lines
 # NOTE: this magic number 2 comes from the fact that 17.3.1-ce.0 version's
@@ -35,15 +16,16 @@ source /tmp/modified_wrapper
 echo "GitLab is up and running. Performing post-launch actions..."
 
 # Create token "root-token" with sudo and api permissions
-RUBY_COMMAND='gitlab-rails runner "token = User.find_by_username(\"root\").personal_access_tokens.create(scopes: [\"api\", \"read_user\", \"read_api\", \"read_repository\", \"write_repository\", \"sudo\", \"admin_mode\"], name: \"root-token\", expires_at: 365.days.from_now); token.set_token(\"root-token\"); token.save!; puts \"Token created successfully.\""'
-retry_command "$RUBY_COMMAND" $MAX_RETRIES $RETRY_DELAY
-
+gitlab-rails runner "token = User.find_by_username('root').personal_access_tokens\
+    .create(scopes: ['api', 'read_user', 'read_api', 'read_repository', 'write_repository', 'sudo', 'admin_mode'], name: 'root-token', expires_at: 365.days.from_now); \
+    token.set_token('root-token'); \
+    token.save!"
 
 # Change configs to enable import from GitLab exports
 # in addition, enable project export
 # TODO: figure out how to allow github imports as well
-CURL_COMMAND='curl --request PUT --header "PRIVATE-TOKEN: root-token" "http://localhost:8929/api/v4/application/settings?import_sources=gitlab_project&project_export_enabled=true"'
-retry_command "$CURL_COMMAND" $MAX_RETRIES $RETRY_DELAY
+curl --request PUT --header "PRIVATE-TOKEN: root-token" \
+    "http://localhost:8929/api/v4/application/settings?import_sources=gitlab_project&project_export_enabled=true"
 
 # Import projects (please make sure they are available under local exports directory)
 # this way, we can build and ship a GitLab image with pre-imported repos
@@ -53,8 +35,11 @@ if ls /assets/exports/*.tar.gz 1> /dev/null 2>&1; then
         filename=$(basename "$file" .tar.gz)
 
         echo "Importing $filename..."
-        IMPORT_COMMAND='curl --request POST --header "PRIVATE-TOKEN: root-token" --form "path=$filename" --form "file=@$file" "http://localhost:8929/api/v4/projects/import"'
-        retry_command "$IMPORT_COMMAND" $MAX_RETRIES $RETRY_DELAY
+        curl --request POST \
+             --header "PRIVATE-TOKEN: root-token" \
+             --form "path=$filename" \
+             --form "file=@$file" \
+             "http://localhost:8929/api/v4/projects/import"
     done
 else
     echo "No .tar.gz file found in /assets/exports/. Nothing to import."
@@ -63,10 +48,13 @@ fi
 echo "Finished importing all repos"
 
 # Create a dedicated project to place all wiki (company-wide doc)
-CREATE_PROJECT_COMMAND='curl --request POST --header "PRIVATE-TOKEN: root-token" --header "Content-Type: application/json" --data \"{"name": "Documentation", "description": "Wiki for company-wide doc", "path": "doc", "wiki_access_level": "enabled", "with_issues_enabled": false, "with_merge_requests_enabled": false, "visibility": "public"}\" --url "http://localhost:8929/api/v4/projects/"'
-retry_command "$CREATE_PROJECT_COMMAND" $MAX_RETRIES $RETRY_DELAY
-
-
+curl --request POST --header "PRIVATE-TOKEN: root-token" \
+     --header "Content-Type: application/json" --data '{
+        "name": "Documentation", "description": "Wiki for company-wide doc", "path": "doc",
+        "wiki_access_level": "enabled", "with_issues_enabled": "false",
+        "with_merge_requests_enabled": "false",
+        "visibility": "public"}' \
+     --url "http://localhost:8929/api/v4/projects/"
 # TODO: change authorship of issues/prs/commits
 
 # Keep the container running
