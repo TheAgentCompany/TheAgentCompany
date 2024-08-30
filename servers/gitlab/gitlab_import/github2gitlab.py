025fc29d4ea9ee685ffb19e8aa98186d201a40d6
Boxuan Li <liboxuan@connect.hku.hk>
2024-08-29 19:02:33 -0700

    Add TODO

--- servers/gitlab/gitlab_import/github2gitlab.py
+++ servers/gitlab/gitlab_import/github2gitlab.py
@@ -1,5 +1,6 @@
 from utils import import_repos
 
 if __name__ == '__main__':
-    # Note: gitlab import is asynchronous and happens in GitLab server background process
+    # Note: github import is asynchronous and happens in GitLab server background process
+    # TODO: move some logic from utils to here, and make helper functions in utils irrelevant to business logic
     import_repos()
