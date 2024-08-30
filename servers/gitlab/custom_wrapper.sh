a62bdec54f439829b54c9afed8fd8738513e55a3
Boxuan Li <liboxuan@connect.hku.hk>
2024-08-30 11:08:50 -0700

    Fixes

--- servers/gitlab/custom_wrapper.sh
+++ servers/gitlab/custom_wrapper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e
+set -ex
 
 MAX_RETRIES=3
-RETRY_DELAY=5
+RETRY_DELAY=10
 
 function retry_command() {
     local command="$1"
@@ -10,7 +10,7 @@ function retry_command() {
     local delay="$3"
     local attempt=1
 
-    until $command
+    until eval "$command"
     do
         if ((attempt == max_attempts))
         then
@@ -22,6 +22,7 @@ function retry_command() {
         sleep $delay
         ((attempt++))
     done
+    echo "Command succeeded"
 }
 
 # Run the original wrapper script, but remove the last five lines
@@ -34,14 +35,15 @@ source /tmp/modified_wrapper
 echo "GitLab is up and running. Performing post-launch actions..."
 
 # Create token "root-token" with sudo and api permissions
-retry_command "gitlab-rails runner \"token = User.find_by_username('root').personal_access_tokens.create(scopes: ['api', 'read_user', 'read_api', 'read_repository', 'write_repository', 'sudo', 'admin_mode'], name: 'root-token', expires_at: 365.days.from_now); token.set_token('root-token'); token.save!\"" $MAX_RETRIES $RETRY_DELAY
+RUBY_COMMAND='gitlab-rails runner "token = User.find_by_username(\"root\").personal_access_tokens.create(scopes: [\"api\", \"read_user\", \"read_api\", \"read_repository\", \"write_repository\", \"sudo\", \"admin_mode\"], name: \"root-token\", expires_at: 365.days.from_now); token.set_token(\"root-token\"); token.save!; puts \"Token created successfully.\""'
+retry_command "$RUBY_COMMAND" $MAX_RETRIES $RETRY_DELAY
 
 
 # Change configs to enable import from GitLab exports
 # in addition, enable project export
 # TODO: figure out how to allow github imports as well
-retry_command "curl --request PUT --header \"PRIVATE-TOKEN: root-token\" \"http://localhost:8929/api/v4/application/settings?import_sources=gitlab_project&project_export_enabled=true\"" $MAX_RETRIES $RETRY_DELAY
-
+CURL_COMMAND='curl --request PUT --header "PRIVATE-TOKEN: root-token" "http://localhost:8929/api/v4/application/settings?import_sources=gitlab_project&project_export_enabled=true"'
+retry_command "$CURL_COMMAND" $MAX_RETRIES $RETRY_DELAY
 
 # Import projects (please make sure they are available under local exports directory)
 # this way, we can build and ship a GitLab image with pre-imported repos
@@ -51,8 +53,8 @@ if ls /assets/exports/*.tar.gz 1> /dev/null 2>&1; then
         filename=$(basename "$file" .tar.gz)
 
         echo "Importing $filename..."
-
-        retry_command "curl --request POST --header \"PRIVATE-TOKEN: root-token\" --form \"path=$filename\" --form \"file=@$file\" \"http://localhost:8929/api/v4/projects/import\"" $MAX_RETRIES $RETRY_DELAY
+        IMPORT_COMMAND='curl --request POST --header "PRIVATE-TOKEN: root-token" --form "path=$filename" --form "file=@$file" "http://localhost:8929/api/v4/projects/import"'
+        retry_command "$IMPORT_COMMAND" $MAX_RETRIES $RETRY_DELAY
     done
 else
     echo "No .tar.gz file found in /assets/exports/. Nothing to import."
@@ -61,10 +63,11 @@ fi
 echo "Finished importing all repos"
 
 # Create a dedicated project to place all wiki (company-wide doc)
-retry_command "curl --request POST --header \"PRIVATE-TOKEN: root-token\" --header \"Content-Type: application/json\" --data '{\"name\": \"Documentation\", \"description\": \"Wiki for company-wide doc\", \"path\": \"doc\", \"wiki_access_level\": \"enabled\", \"with_issues_enabled\": \"false\", \"with_merge_requests_enabled\": \"false\", \"visibility\": \"public\"}' --url \"http://localhost:8929/api/v4/projects/\"" $MAX_RETRIES $RETRY_DELAY
+CREATE_PROJECT_COMMAND='curl --request POST --header "PRIVATE-TOKEN: root-token" --header "Content-Type: application/json" --data \"{"name": "Documentation", "description": "Wiki for company-wide doc", "path": "doc", "wiki_access_level": "enabled", "with_issues_enabled": false, "with_merge_requests_enabled": false, "visibility": "public"}\" --url "http://localhost:8929/api/v4/projects/"'
+retry_command "$CREATE_PROJECT_COMMAND" $MAX_RETRIES $RETRY_DELAY
 
 
 # TODO: change authorship of issues/prs/commits
 
-echo "Setup completed successfully"
-exit 0
+# Keep the container running
+wait
