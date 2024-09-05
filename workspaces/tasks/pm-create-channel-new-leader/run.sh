43e1cc9f8a34da151d67f3be659ce65ec5e9cb8c
ljang0 <ljang@andrew.cmu.edu>
2024-09-05 03:51:34 -0400

    new task

---
+++ workspaces/tasks/pm-create-channel-new-leader/run.sh
@@ -0,0 +1,10 @@
+# do initialization
+python initialization.py
+
+python functionality.py
+
+# sleep and run evaluactor
+sleep 10 && python evaluator.py
+
+# keep container running
+tail -f /dev/null
