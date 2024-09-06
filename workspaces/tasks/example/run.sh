75f764ada463f252c896840d4b343e6526da841c
Boxuan Li <liboxuan@connect.hku.hk>
2024-09-05 21:22:15 -0700

    Add back init.py & init.sh to example

--- workspaces/tasks/example/run.sh
+++ workspaces/tasks/example/run.sh
@@ -1,9 +1,3 @@
 # do initialization
-sh initialization.sh
-python initialization.py
-
-# sleep and run evaluactor
-sleep 10 && python evaluator.py
-
-# keep container running
-tail -f /dev/null
+sh init.sh
+python init.py
