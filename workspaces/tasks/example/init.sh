09c00f8aa6382c57f6a34916fd46bdb2030164bd
yufansong <yufansong98@gmail.com>
2024-09-10 03:32:18 -0400

    revert change

--- workspaces/tasks/example/init.sh
+++ workspaces/tasks/example/init.sh
@@ -8,11 +8,23 @@ fi
 
 
 
-################### Example about Launch NPC ############################
+################### Example about block host ############################
 ############### Please delete it if you don't need ######################
 
 
 BLOCKED_DOMAINS=${BLOCKED_DOMAINS:-"youtube.com reddit.com"}
 
-python /npc/run_multi_npc.py
+cp /etc/hosts /etc/hosts.backup
+
+for domain in $BLOCKED_DOMAINS; do
+    if ! grep -q "$domain" /etc/hosts; then
+        echo "127.0.0.1 $domain" >> /etc/hosts
+        echo "127.0.0.1 www.$domain" >> /etc/hosts
+        echo "Blocked: $domain"
+    else
+        echo "$domain is already in hosts file"
+    fi
+done
+
+rm /etc/hosts.backup
 echo "Hosts file updated successfully"
