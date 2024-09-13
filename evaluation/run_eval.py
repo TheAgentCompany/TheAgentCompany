3778dc06e764b42d3ea1e68c9b7a33d6495bf7de
Boxuan Li <liboxuan@connect.hku.hk>
2024-09-13 01:04:07 -0700

    Add evaluation code using OpenHands

--- evaluation/run_eval.py
+++ evaluation/run_eval.py
@@ -1,7 +1,11 @@
+import asyncio
+import os
+
 from openhands.controller.state.state import State
 from openhands.core.config import (
     AppConfig,
     SandboxConfig,
+    LLMConfig,
     get_llm_config_arg,
     get_parser,
 )
@@ -12,12 +16,58 @@ from openhands.events.observation import CmdOutputObservation, ErrorObservation
 from openhands.runtime.runtime import Runtime
 
 
+def get_config(
+    base_container_image: str,
+    llm_config: LLMConfig
+) -> AppConfig:
+    config = AppConfig(
+        run_as_openhands=False,
+        max_budget_per_task=4,
+        sandbox=SandboxConfig(
+            base_container_image=base_container_image,
+            enable_auto_lint=True,
+            use_host_network=False,
+            # large enough timeout, since some testcases take very long to run
+            timeout=300,
+            api_key=os.environ.get('ALLHANDS_API_KEY', None),
+        ),
+        # do not mount workspace
+        workspace_base=None,
+        workspace_mount_path=None,
+    )
+    config.set_llm_config(llm_config)
+    return config
+
+
 if __name__ == '__main__':
     parser = get_parser()
     parser.add_argument(
-        '--task',
+        '--task_image_name',
         type=str,
-        default='example',
-        help='Task to evaluate on',
+        default='ghcr.io/neulab/TheAgentCompany_Example',
+        help='Task image name',
     )
     args, _ = parser.parse_known_args()
+
+    llm_config = None
+    if args.llm_config:
+        llm_config = get_llm_config_arg(args.llm_config)
+
+    if llm_config is None:
+        raise ValueError(f'Could not find LLM config: --llm_config {args.llm_config}')
+
+    logger.info(f"Task image name is {args.task_image_name}")
+    config: AppConfig = get_config(args.task_image_name, llm_config)
+    runtime = create_runtime(config)
+
+    instruction = "Complete the task in /instruction/task.md"
+
+    state: State | None = asyncio.run(
+        run_controller(
+            config=config,
+            task_str=instruction,
+            runtime=runtime,
+        )
+    )
+
+    # TODO: run evaluator
