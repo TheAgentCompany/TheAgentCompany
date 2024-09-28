e9b8e669021533b04451da4ef83dfa845218bbe3
Boxuan Li <liboxuan@connect.hku.hk>
2024-09-28 11:24:31 -0700

    Simplify get repo id

--- servers/gitlab/gitlab_import/utils.py
+++ servers/gitlab/gitlab_import/utils.py
@@ -4,6 +4,9 @@ import os
 import logging
 import time
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 # Note: if you use the GitLab image we build, then 'root-token' is already
 # set up. Otherwise, please set up GitLab token by yourself.
 GITLAB_ACCESS_TOKEN = os.getenv('GITLAB_TOKEN', 'root-token')
@@ -21,7 +24,7 @@ GITHUB_HEADER = {
 
 def _check_status_code(status_code):
     if status_code != 200:
-        logging.warning(f'API call status is {status_code}, sleep for 10 seconds...')
+        logger.warning(f'API call status is {status_code}, sleep for 10 seconds...')
         time.sleep(10)
 
 def get_github_profile(username):
@@ -54,13 +57,6 @@ def create_user(username, name, email=None, bio=None, loc=None, org=None):
     r = requests.post(url, json=body, headers=ROOT_HEADER)
     resp = json.loads(r.text)
     return int(resp['id']) if 'id' in resp else -1
-
-def get_public_repos(username):
-    github_url = f'https://api.github.com/users/{username}/repos'
-    r = requests.get(github_url, headers=GITHUB_HEADER)
-    _check_status_code(r.status_code)
-    resp = json.loads(r.text)
-    return [(info['name'], info['id']) for info in resp]
 
 def mirror(username, repo_id):
     mirror_url = f'http://{HOSTNAME}:{PORT}/api/v4/import/github'
@@ -134,30 +130,21 @@ def create_users_from_issues(username, repo):
     
     return users_list
     
-def delete_project(username, repo):
-    delete_url = f'http://{HOSTNAME}:{PORT}/api/v4/projects/{username}%2F{repo}'
-    body = {
-        'id': f'{username}%2F{repo}'
-    }
-    r = requests.delete(delete_url, json=body, headers=ROOT_HEADER)
-    print(json.loads(r.text))
+def get_github_repo_id(USERNAME, REPO):
+    repo_url = f'http://api.github.com/repos/{USERNAME}/{REPO}'
+    r = requests.get(repo_url, headers=GITHUB_HEADER)
+    _check_status_code(r.status_code)
+    resp = json.loads(r.text)
+    return resp['id']
 
 def import_repos(repos):
-    logging.info(f'Gitlab Access Token Used: {GITLAB_ACCESS_TOKEN}')
-    logging.info(f'Github Access Token Used: {GITHUB_ACCESS_TOKEN}') 
+    logger.info(f'Gitlab Access Token Used: {GITLAB_ACCESS_TOKEN}')
+    logger.info(f'Github Access Token Used: {GITHUB_ACCESS_TOKEN}') 
     
     for USERNAME, REPO in repos:
         # username might be a person, or an org
-        repos = get_public_repos(USERNAME)
-        REPO_ID = -1
-        for proj, repo_id in repos:
-            if proj == REPO:
-                REPO_ID = repo_id
-        
-        if REPO_ID < 0:
-            logging.error(f'Repo: {REPO} Not Found for {USERNAME}!!')
-            continue
-        
+        REPO_ID = get_github_repo_id(USERNAME, REPO)
+        logger.info(f'Repo id for {USERNAME}/{REPO}: {REPO_ID}')
         users_list = create_users_from_pulls(USERNAME, REPO)
         users_list.extend(create_users_from_issues(USERNAME, REPO))
         users_list = list(set(users_list))
