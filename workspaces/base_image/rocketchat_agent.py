e42b5122cefe436f7a470a1ed9a8daab5b5fa833
yufansong <yufansong98@gmail.com>
2024-10-14 06:22:55 -0400

    fix full name bug

--- workspaces/base_image/rocketchat_agent.py
+++ workspaces/base_image/rocketchat_agent.py
@@ -73,7 +73,7 @@ class RocketChatAgent(BaseAgent[Observation, AgentAction]):
         uuid_str: str | None = None,
         session_id: str | None = None,
         agent_profile: AgentProfile | None = None,
-        first_name: str | None = None,
+        credential_name: str | None = None,
     ) -> None:
         super().__init__(
             agent_name=agent_name,
@@ -83,8 +83,13 @@ class RocketChatAgent(BaseAgent[Observation, AgentAction]):
         # super().__init__(agent_name=agent_name, uuid_str=uuid_str)
         self.session_id = session_id or str(uuid4())
         self.sender_id = str(uuid4())
-        print(f"step 1: connect to the server: user first name: {first_name}")
-        username, password = get_credentials(first_name)
+        print(f"step 1: connect to the server: user first name: {credential_name}")
+        # TODO: remove this hack, credential_name are not the same agent with agent_profile
+        if agent_profile.first_name == "X":
+            username, password = get_credentials(credential_name)
+        else:
+            username, password = get_credentials(agent_profile.first_name+" "+agent_profile.last_name)
+        print(username, password)
         self.bot = RocketChatBot(username, password, server_url)
         self.send_init_message()
         logging.info(f"Session ID: {self.session_id}")
