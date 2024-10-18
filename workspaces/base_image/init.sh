3c83715a38034d962217f19b0571f15e76e8cb25
Boxuan Li <liboxuan@connect.hku.hk>
2024-10-18 00:29:49 -0700

    Use theagentcompany.com synthetic domain except for nextcloud

--- workspaces/base_image/init.sh
+++ workspaces/base_image/init.sh
@@ -3,7 +3,8 @@ set -ex
 
 ########## PRE INIT PHASE ############
 
-# Use synthetic service hostname in tasks and point it to your real service host
+# Use synthetic service hostname, theagentcompany.com in tasks and point it
+# to your real service host
 SERVER_HOSTNAME=${SERVER_HOSTNAME:-ogma.lti.cs.cmu.edu}
 SERVICE_IP=$(ping -c 1 $SERVER_HOSTNAME | grep PING | awk -F'[()]' '{print $2}')
 echo "$SERVICE_IP theagentcompany.com" >> /etc/hosts
