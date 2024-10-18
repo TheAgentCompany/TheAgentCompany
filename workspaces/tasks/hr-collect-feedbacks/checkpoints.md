cce2010d58dc1da082e948f366aa002729e3f6d8
Andy Lu <andylu@Mac.home>
2024-10-17 23:06:04 -0500

    benchmark for collecting feedback

---
+++ workspaces/tasks/hr-collect-feedbacks/checkpoints.md
@@ -0,0 +1,18 @@
+# Checkpoints
+This task has 5 points in total. If the last check passes, then full credits are granted. Otherwise, partial credits might be granted if only some checkpoints are completed.
+
+## Checkpoint 1 (1pt)
+
+Confirmed checked Liu Qiang's job performance with Mike Chen
+
+## Checkpoint 1 (1pt)
+
+Confirmed checked Liu Qiang's job performance with Li Ming
+
+## Checkpoint 1 (1pt)
+
+Confirmed checked Liu Qiang's job performance with Zhang Wei
+
+## Checkpoint 2 (2pt)
+
+Confirmed provided correct feedback to Liu Qiang about his job performance
---
+++ workspaces/tasks/hr-collect-feedbacks/evaluator.py
@@ -0,0 +1,92 @@
+import os
+from rocketchat_API.rocketchat import RocketChat
+
+from llm_evaluator import *
+
+############################# Init Variables #####################################
+# Rocket.Chat variables
+SERVER_HOSTNAME = os.getenv('SERVER_HOSTNAME') or 'ogma.lti.cs.cmu.edu'
+
+
+ROCKETCHAT_PORT = os.getenv('ROCKETCHAT_PORT') or '3000'
+
+
+ROCKETCHAT_URL = f"http://{SERVER_HOSTNAME}:{ROCKETCHAT_PORT}"
+ADMIN_USERNAME = 'jobbench'
+ADMIN_PASS = 'jobbench'
+
+# Plane variables
+PLANE_HOSTNAME = os.getenv('PLANE_HOSTNAME') or 'ogma.lti.cs.cmu.edu'
+
+PLANE_PORT =  os.getenv('PLANE_PORT') or '8091'
+
+PLANE_BASEURL = f"http://{PLANE_HOSTNAME}:{PLANE_PORT}"
+PLANE_WORKSPACE_SLUG = os.getenv("PLANE_WORKSPACE_SLUG") or "cmu"
+
+API_KEY = os.getenv('PLANE_API') 
+
+
+headers = {
+    "x-api-key": API_KEY,
+    "Content-Type": "application/json"
+}
+
+# Initialize RocketChat client
+rocket = RocketChat(ADMIN_USERNAME, ADMIN_PASS, server_url=ROCKETCHAT_URL)
+
+############################# Helper Functions #####################################
+
+def get_history(username: str):
+    id = ""
+    for item in rocket.users_list().json()['users']:
+        if item['nameInsensitive'] == username.lower():
+            id = item["_id"]
+            break
+    msgs = rocket.im_history(room_id=id).json()['messages']
+    for msg in msgs[::-1]:
+        print(f"{username}'s chat: {msg['msg']}")
+
+    if len(msgs)>0:
+        return True, msg
+    else:
+        return False, msg
+
+
+def check_final_result(username: str):
+
+    _, msg = get_history(username)
+
+    messages = [{"content": f"Does the phrase \"\"{msg}\"\" indicate the person receiving the feedback had an average job performance? Please answer 'yes' if it does, or 'no' if it doesn't.", "role": "user"}]
+    # Check result
+    llm_resonse = llm_evaluator(messages).json()
+    print(llm_resonse)
+
+    if 'yes' in llm_resonse['choices'][0]['message']['content'].lower():
+        print(f"{username} received feedback that indicates an average job performance")
+        return True
+    else:
+        print(f"{username} did not receive feedback that indicates an average job performance")
+        return False
+
+############################# Evaluator #####################################
+
+def evaluate_task():
+    points = 0
+
+    npcs = ["Mike Chen", "Li Ming", "Zhang Wei"]
+    performance_evaluatee = "Liu Qiang"
+
+    for npc in npcs:
+        res, _ = get_history(npc)
+        if res:
+            points += 1
+
+    if check_final_result(performance_evaluatee):
+        points += 2
+
+    # Final result
+    print(f"Evaluation completed. Final score: {points}/5")
+    return points
+
+if __name__ == "__main__":
+    evaluate_task()
