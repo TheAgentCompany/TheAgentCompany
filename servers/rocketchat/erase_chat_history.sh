6b7a1fb367a7b53b8d1a4f11ed333345cb92d0fc
yufansong <yufansong98@gmail.com>
2024-10-19 07:31:06 -0400

    add script to erase the chat history

--- servers/rocketchat/erase_chat_history.sh
+++ servers/rocketchat/erase_chat_history.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-# Calculate the date one year from now
-FUTURE_DATE=$(date -d "+1 year" +%Y-%m-%d)
-echo "Messages before $FUTURE_DATE will be deleted"
-
 # Execute MongoDB commands in the Docker container
+# We will delete all user and all messages
 docker exec servers-mongodb-1 mongosh --eval "
   db = db.getSiblingDB('rocketchat');
-  var futureDate = new Date('$FUTURE_DATE');
-  db.rocketchat_message.deleteMany({ ts: { \$lt: futureDate } });
+  db.rocketchat_message.deleteMany({});
+  db.users.deleteMany({});
   print('Deletion completed');
 "
+
+
+# Keep the follow code in case we want to keep some message, then we can use the time to control message, but cannot control users
+# Calculate the date one year from now
+# FUTURE_DATE=$(date -d "+1 year" +%Y-%m-%d)
+# echo "Messages before $FUTURE_DATE will be deleted"
+# db.rocketchat_message.deleteMany({ ts: { \$lt: futureDate } });
