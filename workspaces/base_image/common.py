4bf3e0f189848176b1951899dc8c20cd30affd88
Boxuan Li <liboxuan@connect.hku.hk>
2024-10-19 22:08:35 -0700

    Add mock rocket chat client in common.py

--- workspaces/base_image/common.py
+++ workspaces/base_image/common.py
@@ -2,6 +2,21 @@ import os
 import logging
 from rocketchat_API.rocketchat import RocketChat
 
+# In test mode, we use mock servers
+TEST_MODE = True
+
+class MockRocketChatClient:
+
+    class JsonResponse:
+        def json(self):
+            return {'users': [], 'messages': []}
+
+    def __getattr__(self, name):
+        def method(*args, **kwargs):
+            return self.JsonResponse()
+        return method
+
+
 def create_rocketchat_client():
     SERVER_HOSTNAME = os.getenv('SERVER_HOSTNAME') or 'the-agent-company.com'
     ROCKETCHAT_PORT = os.getenv('ROCKETCHAT_PORT') or '3000'
@@ -17,4 +32,7 @@ def create_rocketchat_client():
         return RocketChat(ADMIN_USERNAME, ADMIN_PASSWORD, server_url=ROCKETCHAT_URL)
     except Exception as e:
         logging.warning("Fail to connect to rocketchat", e)
-        return None
+        if TEST_MODE:
+            return MockRocketChatClient()
+        else:
+            raise
