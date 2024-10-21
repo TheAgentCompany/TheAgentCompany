70b81e0e64375ee1a0f68afc134a796804355cbb
Boxuan Li <liboxuan@connect.hku.hk>
2024-10-20 20:56:03 -0700

    Run test mode only in CI

--- workspaces/base_image/common.py
+++ workspaces/base_image/common.py
@@ -5,12 +5,12 @@ import litellm
 
 from rocketchat_API.rocketchat import RocketChat
 
-# In test mode, we use mock servers
-TEST_MODE = True
+# In test mode, we use mock servers and mock LLM responses
+TEST_MODE = os.environ.get('TAC_TEST_MODE', False)
 
 LITELLM_API_KEY = os.environ.get("LITELLM_API_KEY")
-LITELLM_BASE_URL = os.environ.get("LITELLM_BASE_URL")
-LITELLM_MODEL = os.environ.get("LITELLM_MODEL")
+LITELLM_BASE_URL = os.environ.get("LITELLM_BASE_URL", "https://api.openai.com/v1")
+LITELLM_MODEL = os.environ.get("LITELLM_MODEL", "gpt-4o")
 
 # messages: a list of message.
 # example [{ "content": "Hello, how are you?","role": "user"}]
