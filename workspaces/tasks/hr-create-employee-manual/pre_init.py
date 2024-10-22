3adbc019d508b0f7b80035d47f995504ed64c082
ZhitongGuo <zhitongg@andrew.cmu.edu>
2024-10-21 21:51:41 -0400

    add task

---
+++ workspaces/tasks/hr-create-employee-manual/pre_init.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+import requests
+import logging
+from requests.auth import HTTPBasicAuth
+import xml.etree.ElementTree as ET
+
+############################# init variable ##################################### 
+HOSTNAME = os.getenv('HOSTNAME') or 'ogma.lti.cs.cmu.edu'
+
+############################# util function #####################################  
+# Set up logging
+logging.basicConfig(level=logging.INFO,    
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        # logging.FileHandler("app.log"),  # Log messages to a file
+        logging.StreamHandler()  # Log messages to the console
+    ])
+logger = logging.getLogger("Functionality Test")
+
+############################# Test function ##################################### 
+
+
+if __name__ == "__main__":
+    pass
