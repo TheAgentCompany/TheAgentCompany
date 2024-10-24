dc36a91035ae6cb860b184fbc3815e6aed444d02
ZhitongGuo <zhitongg@andrew.cmu.edu>
2024-10-24 01:16:26 -0400

    add nextcloud url in config

--- workspaces/base_image/config.py
+++ workspaces/base_image/config.py
@@ -5,5 +5,6 @@ TEST_MODE = os.environ.get('TAC_TEST_MODE', False)
 LITELLM_API_KEY = os.environ.get("LITELLM_API_KEY")
 LITELLM_BASE_URL = os.environ.get("LITELLM_BASE_URL", "https://api.openai.com/v1")
 LITELLM_MODEL = os.environ.get("LITELLM_MODEL", "gpt-4o")
+NEXTCLOUD_URL = os.environ.get("NEXTCLOUD_URL", "https://ogma.lti.cs.cmu.edu")
 
 PLANE_API_KEY = os.environ.get("PLANE_API_KEY") or "plane_api_569b8e604e0c46d0b65ef56bb9e76f03"
