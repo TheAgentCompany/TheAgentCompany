2581ce319447b68a04fb81cfd1517c8156988062
Murong Cao <apyanhui@gmail.com>
2024-10-23 21:13:20 -0700

    Remove dead code

--- workspaces/tasks/hr-resume-screening/evaluator.py
+++ workspaces/tasks/hr-resume-screening/evaluator.py
@@ -1,31 +1,9 @@
-import logging
-
 from common import create_rocketchat_client
 from common import evaluate_chat_history_with_llm
 from common import get_chat_history
 
 # Initialize RocketChat client
 rocket = create_rocketchat_client()
-
-def check_final_result(username: str):
-    success, msg = get_chat_history(username)
-
-    if not success:
-        logging.warning(f"Failed to get history or no history")
-        return False
-
-    messages = [{"content": f"Does the message \"\"{msg}\"\" indicate Jane Lee cannot be hired because she fails to meet visa/green card/citizenship requirement? Please answer 'yes' if it does, or 'no' if it doesn't.", "role": "user"}]
-    # Check result
-    llm_resonse = llm_evaluate(messages)
-    logging.info('LLM evaluator response is', llm_resonse)
-
-    if 'yes' in llm_resonse['choices'][0]['message']['content'].lower():
-        logging.info(f"{username} received expected conclusion")
-        return True
-    else:
-        logging.info(f"{username} didn't receive expected conclusion")
-        return False
-
 
 def run_checkpoint_1():
     msgs = get_chat_history(rocket, 'Chen Xinyi')
