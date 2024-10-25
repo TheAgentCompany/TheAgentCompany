88da1973c584ba888c66a373341f0efc00c81ae5
Yuxuan <AlanMacBook@tangyuxuandeMacBook-Pro.local>
2024-10-25 14:17:53 -0700

    small change

--- workspaces/base_image/config.py
+++ workspaces/base_image/config.py
@@ -2,11 +2,12 @@ import os
 # In test mode, we use mock servers and mock LLM responses
 TEST_MODE = os.environ.get('TAC_TEST_MODE', False)
 
+SERVER_HOSTNAME = os.getenv('SERVER_HOSTNAME') or 'the-agent-company.com'
+
+# LLM Config
 LITELLM_API_KEY = os.environ.get("LITELLM_API_KEY")
 LITELLM_BASE_URL = os.environ.get("LITELLM_BASE_URL", "https://api.openai.com/v1")
 LITELLM_MODEL = os.environ.get("LITELLM_MODEL", "gpt-4o")
-
-SERVER_HOSTNAME = os.getenv('SERVER_HOSTNAME') or 'the-agent-company.com'
 
 # Rocketchat Config
 ROCKETCHAT_PORT = os.getenv('ROCKETCHAT_PORT') or '3000'
