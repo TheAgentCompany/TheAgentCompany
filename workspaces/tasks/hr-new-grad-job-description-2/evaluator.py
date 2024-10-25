afafea8c7800c7f5d482d1fb5031f6ad47d86caa
Hao Yang (Andy) Lu <andyluswc@gmail.com>
2024-10-25 03:08:02 -0500

    address comments

--- workspaces/tasks/hr-new-grad-job-description-2/evaluator.py
+++ workspaces/tasks/hr-new-grad-job-description-2/evaluator.py
@@ -1,97 +1,20 @@
-from common import llm_evaluate, create_rocketchat_client
-import subprocess
-import re
-import os
+from common import llm_evaluate, create_rocketchat_client, get_chat_history, get_nextcloud_url_in_file, download_nextcloud_content
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-# Set environment variables
-SERVER_HOSTNAME = os.getenv('SERVER_HOSTNAME') or 'the-agent-company.com'
-ROCKETCHAT_PORT = os.getenv('ROCKETCHAT_PORT') or '3000'
-
-# Construct RocketChat URL
-ROCKETCHAT_URL = f"http://{SERVER_HOSTNAME}:{ROCKETCHAT_PORT}"
 
 # Create RocketChat instance
 rocket = create_rocketchat_client()
 
 ############################# Helper Functions #####################################
 
-def get_history(username):
-    id = ""
-    for item in rocket.users_list().json()['users']:
-        if item['username'] == username:
-            id = item["_id"]
-            break
-    msgs = rocket.im_history(room_id=id).json()['messages']
-    for msg in msgs[::-1]:
-        logger.info(f"{username}'s chat: {msg['msg']}")
-
-    if len(msgs)>0:
-        return True
-    else:
-        return False
-
-
-def check_url_file_exist():
-    filename = "/workspace/link.txt"
-    try:
-        with open(filename, 'r') as file:
-            content = file.read()
-            if f"https://ogma.lti.cs.cmu.edu" in content:
-                return content
-            return False
-    except FileNotFoundError:
-        logger.error(f"Error: The file '{filename}' was not found.")
-        return False
-    except IOError as e:
-        logger.error(f"Error: An I/O error occurred. Details: {e}")
-        return False
-
-def verify_nextcloud_content_exists(link):
-
-    if "download" not in link:
-        command = ["curl", "--output", "/workspace/download_link", link.rstrip("\n")]
-        try:
-            subprocess.run(command)
-        except Exception as e:
-            logger.error(f"Unable to download from link: {link}")
-            return False
-
-        pattern = r'https?://[^\s]*\bdownload\b[^\s]*(?=")'
-
-        download_link: str | None = None
-        with open("/workspace/download_link", "r") as f:
-            content = f.read()
-            matches = re.findall(pattern, content, re.MULTILINE)
-            if matches:
-                download_link = matches[0]
-        
-        if download_link is None:
-            logger.error(f"Did not find proper download link")
-            return False
-    else:
-        download_link = link.rstrip("\n")
-
-    try:
-        logger.info(download_link)
-        subprocess.run([f"curl {download_link} --output /workspace/job_description.md"], shell=True)
-    except Exception as e:
-        logger.error(f"Download from link: {download_link} not successful")
-        return False
-
-    logger.info(f"Successfully downloaded from link {download_link}")
-    return True
-
-
 def check_job_description_content():
 
     with open("/workspace/job_description.md", "r") as f:
         final_content = f.read()
     
-    with open("/workspace/template.md", "r") as f:
+    with open("/instruction/template.md", "r") as f:
         template_content = f.read()
     
     with open("/utils/requirement.txt", "r") as f:
@@ -117,13 +40,13 @@ def check_job_description_content():
 def evaluate_task():
     points = 0
 
-    if get_history("zhang_wei"):
+    if get_chat_history(rocket, "zhang_wei"):
         points += 1
 
-    link = check_url_file_exist()
+    link = get_nextcloud_url_in_file("/workspace/link.txt")
     if link:
         points += 1
-        if verify_nextcloud_content_exists(link):
+        if download_nextcloud_content(link, "/workspace/job_description.md"):
             points += 1
 
         if check_job_description_content():
--- workspaces/tasks/hr-new-grad-job-description-2/task.md
+++ workspaces/tasks/hr-new-grad-job-description-2/task.md
@@ -1,4 +1,4 @@
-You need to write a new grad software engineer job description, The template is a Markdown file located under the /workspace directory called template.md. 
+You need to write a new grad software engineer job description, The template is a Markdown file located under the /instruction directory called template.md.
 
 * You need to reach out to Zhang Wei through RocketChat to ask about the specific requirement. Navigate to the Rocketchat web at http://the-agent-company.com:3000/home.
 * You need to merge them and create a file called "job_description.md" on NextCloud under the document directory. You can navigate to NextCloud through the url https://ogma.lti.cs.cmu.edu/apps/dashboard/. Make sure it is a Markdown file.
