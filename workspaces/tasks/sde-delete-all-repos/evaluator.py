ac5a954833dc6af9941f7cdc2ff19132c84214ef
Murong Cao <apyanhui@gmail.com>
2024-10-29 00:42:39 -0700

    Add task: Delete all gitlab repos

---
+++ workspaces/tasks/sde-delete-all-repos/evaluator.py
@@ -0,0 +1,28 @@
+import logging
+from typing import List
+
+from scoring import Result, Checkpoint
+from common import make_gitlab_request
+
+
+def check_all_repositories_deleted():
+    try:
+        # Get all projects
+        response = make_gitlab_request(additional_path="projects").json()
+
+        if len(response) == 0:
+            logging.info(f"All repositories are deleted.")
+            return True
+        else:
+            logging.warning(f"Some repositories are not deleted: {response}")
+            return False
+    except Exception as e:
+        logging.error(f"Error occurred while checking the repository: {e}")
+        return False
+
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints)
+    checkpoints.append(Checkpoint(1, check_all_repositories_deleted()))
+    return result
