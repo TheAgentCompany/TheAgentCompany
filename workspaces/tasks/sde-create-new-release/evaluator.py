9ec8c7de19492cd9d3c2389c22395950ffa5557a
Hao Yang (Andy) Lu <andyluswc@gmail.com>
2024-10-30 22:01:22 -0500

    break into two checkpoints

--- workspaces/tasks/sde-create-new-release/evaluator.py
+++ workspaces/tasks/sde-create-new-release/evaluator.py
@@ -21,32 +21,35 @@ SQL commands:
 Breaking change: DECLARE cursor_name SUBSCRIPTION CURSOR is the same as DECLARE cursor_name SUBSCRIPTION CURSOR since now(), which will be consumed from the current time. DECLARE cursor_name SUBSCRIPTION CURSOR FULL will start consuming data from stock. The type of operation has changed to varchar. It is one of Insert, Delete, UpdateInset, or UpdateDelete.
 """
 
-def check_release_content():
-    total_point = 0
-
+def get_most_recent_release():
     release_encoded_path = urllib.parse.quote(release_path, safe='')
     try:
         releases = make_gitlab_request(project_path, release_encoded_path)
     except requests.RequestException as e:
         logging.error(f"Error occurred while checking releases: {e}")
-        return total_point
+        return None
 
     if not releases:
         logging.warning("Release does not exist")
-        return total_point
+        return None
+    return releases.json()[0]
 
-    most_recent_release = releases.json()[0]
-    title = most_recent_release["name"]
+
+def check_release_title():
+    most_recent_release = get_most_recent_release()
+    if most_recent_release is None:
+        return False
+    if most_recent_release['title'] == "release-2024-10-12":
+        return True
+    return False
+
+def check_release_content():
+    most_recent_release = get_most_recent_release()
     description = most_recent_release["description"]
-
-    total_point = 0
-    if title == "release-2024-10-12":
-        total_point += 1
     
     if description.strip("\n").strip(" ") == release_description.strip("\n").strip(" "):
-        total_point += 1
-
-    return total_point
+        return True
+    return False
 
 
 ############################# Evaluator #####################################
@@ -55,7 +58,9 @@ def grade_checkpoints(trajectory="") -> Result:
     checkpoints: List[Checkpoint] = []
     result = Result(checkpoints)
 
-    checkpoints.append(Checkpoint(2, int(check_release_content())))
+    checkpoints.append(Checkpoint(1, int(check_release_content())))
+
+    checkpoints.append(Checkpoint(1, int(check_release_content())))
 
     return result
 
