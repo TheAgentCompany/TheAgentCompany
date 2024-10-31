264c9f78144a250621bc37868f501efe04f487ea
zorazrw <zhiruo.zora.wang@gmail.com>
2024-10-31 08:43:43 -0400

    make more rigorous, structured tests

--- workspaces/tasks/hr-organize-talent-info/checkpoints.md
+++ workspaces/tasks/hr-organize-talent-info/checkpoints.md
@@ -2,12 +2,16 @@
 
 ## Checkpoint 1 (1pt)
 
-Check all required information for Emily Johnson is included in the summary.txt file.
+Confirmed exists file `summary.csv`.
 
-## Checkpint 2 (1pt)
+## Checkpoint 2 (1pt)
 
-Check all required information for Michael Davis is included in the summary.txt file.
+Check all required information for Emily Johnson is included in the summary.csv file.
 
 ## Checkpint 3 (1pt)
 
-Check all required information for Sarah Thompson is included in the summary.txt file.
+Check all required information for Michael Davis is included in the summary.csv file.
+
+## Checkpint 4 (1pt)
+
+Check all required information for Sarah Thompson is included in the summary.csv file.
--- workspaces/tasks/hr-organize-talent-info/evaluator.py
+++ workspaces/tasks/hr-organize-talent-info/evaluator.py
@@ -1,70 +1,76 @@
 import os
+import csv
 from typing import List
 from scoring import Result, Checkpoint
 
 
-def get_summary_file_content():
-    filepath = "summary.txt"
-    try:
-        with open(filepath, 'r') as f:
-            result = f.read()
-            return result
-    except Exception as e:
-        print(f"Error processing file {filepath}: {e}")
-        return ""
+def check_exists_file():
+    return os.path.exists("/workspace/summary.csv")
+
+def get_summary_file_content(name: str = None):
+    if not os.path.exists("/workspace/summary.csv"):
+        return []
+    rows = []
+    with open("summary.csv") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            if name is not None and row[0] != name:
+                continue
+            rows.append(row)
+    return rows
 
 
 def check_emily_info():
-    file_content = get_summary_file_content()
-    required_content = [
-        "Emily Johnson",
-        "University of California, Berkeley",
-        "Google", "Amazon",
-        "Python", "Java, C++", "JavaScript",
-        "React", "Node.js", "Flask",
-        "Git", "Docker", "AWS", "MySQL",
-    ]
-    if all([rc in file_content for rc in required_content]):
-        return 1
-    else:
+    rows = get_summary_file_content(name="Emily Johnson")
+    if len(rows) != 1:
         return 0
+    name, education, work, skills = rows[0]
+    if name != "Emily Johnson": return 0
+    if "Berkeley" not in education: return 0
+    if any([c not in work for c in ["Google", "Amazon"]]): return 0
+    if any([c not in skills for c in [
+        "Python", "Java, C++", "JavaScript", "React", "Node.js", 
+        "Flask", "Git", "Docker", "AWS", "MySQL"
+    ]]): return 0
+    return 1
 
 
 def check_michael_info():
-    file_content = get_summary_file_content()
-    required_content = [
-        "Michael Davis",
-        "New York University",
-        "Meta", "Spotify",
+    rows = get_summary_file_content(name="Michael Davis")
+    if len(rows) != 1:
+        return 0
+    name, education, work, skills = rows[0]
+    if name != "Michael Davis": return 0
+    if "New York University" not in education: return 0
+    if any([c not in work for c in ["Meta", "Spotify"]]): return 0
+    if any([c not in skills for c in [
         "JavaScript", "Python", "Swift", "Ruby",
         "Django", "Angular", "React Native",
         "Git", "Firebase", "PostgreSQL", "Jenkins",
-    ]
-    if all([rc in file_content for rc in required_content]):
-        return 1
-    else:
-        return 0
+    ]]): return 0
+    return 1
 
 
 def check_sarah_info():
-    file_content = get_summary_file_content()
-    required_content = [
-        "Sarah Thompson",
-        "University of Texas at Austin",
-        "Microsoft", "Oracle",
+    rows = get_summary_file_content(name="Sarah Thompson")
+    if len(rows) != 1:
+        return 0
+    name, education, work, skills = rows[0]
+    if name != "Sarah Thompson": return 0
+    if "University of Texas at Austin" not in education: return 0
+    if any([c not in work for c in ["Microsoft", "Oracle"]]): return 0
+    if any([c not in skills for c in [
         "Python", "Java", "C#", "HTML/CSS",
         "Angular", "Spring Boot", "Express.js",
         "Git", "Docker", "MongoDB", "Kubernetes",
-    ]
-    if all([rc in file_content for rc in required_content]):
-        return 1
-    else:
-        return 0
+    ]]): return 0
+    return 1
 
 
 def grade_checkpoints(trajectory=""):
     checkpoints: List[Checkpoint] = []
     result = Result(checkpoints)
+    checkpoints.append(Checkpoint(1, int(check_exists_file())))
     checkpoints.append(Checkpoint(1, check_emily_info()))
     checkpoints.append(Checkpoint(1, check_michael_info()))
     checkpoints.append(Checkpoint(1, check_sarah_info()))
