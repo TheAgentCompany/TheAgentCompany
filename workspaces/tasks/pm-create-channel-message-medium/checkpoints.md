62241aade13452a7b904a055553995fb53d3247f
ljang0 <ljang@andrew.cmu.edu>
2024-11-05 01:30:47 -0500

    refactored my task

--- workspaces/tasks/pm-create-channel-message-medium/checkpoints.md
+++ workspaces/tasks/pm-create-channel-message-medium/checkpoints.md
@@ -12,33 +12,41 @@ Created the public #literaturereview channel (http://ogma.lti.cs.cmu.edu:3000/ch
 
 ## Checkpoint 3 (1pt)
 
-Invited Emma Lewis
+Navigate to the public #literaturereview channel (http://ogma.lti.cs.cmu.edu:3000/channel/literaturereview)
 
 ## Checkpoint 4 (1pt)
 
-Invited Wang Fang
+Invited Emma Lewis
 
 ## Checkpoint 5 (1pt)
 
-Sent message 'Welcome to the literature review channel!'
+Invited Wang Fang
 
 ## Checkpoint 6 (1pt)
 
-Created the public #benchmarking channel (http://ogma.lti.cs.cmu.edu:3000/channel/benchmarking) 
+Sent message 'Welcome to the literature review channel!'
 
 ## Checkpoint 7 (1pt)
 
-Invited Emma Lewis
+Created the public #benchmarking channel (http://ogma.lti.cs.cmu.edu:3000/channel/benchmarking) 
 
 ## Checkpoint 8 (1pt)
 
-Invited Alex Turner
+Navigate to the public #benchmarking channel (http://ogma.lti.cs.cmu.edu:3000/channel/benchmarking) 
 
 ## Checkpoint 9 (1pt)
 
-Invited David Wong
+Invited Emma Lewis
 
 ## Checkpoint 10 (1pt)
+
+Invited Alex Turner
+
+## Checkpoint 11 (1pt)
+
+Invited David Wong
+
+## Checkpoint 12 (1pt)
 
 Sent message "This will be for all our benchmarking projects."
 
--- workspaces/tasks/pm-create-channel-message-medium/evaluator.py
+++ workspaces/tasks/pm-create-channel-message-medium/evaluator.py
@@ -1,27 +1,17 @@
-import requests
 import os
-from rocketchat_API.rocketchat import RocketChat
-import ipdb
+
+from typing import List
+
+from scoring import Result, Checkpoint, bonus_for_completing_final
+from common import create_rocketchat_client
 
 # Rocket.Chat server URL and admin credentials
-SERVER_HOSTNAME = 'the-agent-company.com'
+SERVER_HOSTNAME = os.getenv('SERVER_HOSTNAME') or 'the-agent-company.com'
 ROCKETCHAT_PORT = os.getenv('ROCKETCHAT_PORT') or '3000'
-channel_name = "general"
 ROCKETCHAT_URL = f"http://{SERVER_HOSTNAME}:{ROCKETCHAT_PORT}"
-ADMIN_USERNAME = 'theagentcompany'
-ADMIN_PASS = 'theagentcompany'
-print(ROCKETCHAT_URL)
-# Initialize the RocketChat client with username and password
-rocket = RocketChat(ADMIN_USERNAME, ADMIN_PASS, server_url=ROCKETCHAT_URL)
 
-def check_url_1(browser_logs):
-    return f"{ROCKETCHAT_URL}/home" in browser_logs
-
-def check_url_2(browser_logs):
-    return f"{ROCKETCHAT_URL}/channel/literaturereview" in browser_logs
-
-def check_url_3(browser_logs):
-    return f"{ROCKETCHAT_URL}/channel/benchmarking" in browser_logs
+# Create RocketChat instance
+rocket = create_rocketchat_client()
 
 def get_channel_room_id(channel_name):
     """Get the room_id for a specific channel by its name."""
@@ -62,27 +52,80 @@ def check_message_sent(channel_name, message, username):
                 return True
     return False
 
+def grade_checkpoint1(trajectory):
+    return f"{ROCKETCHAT_URL}/home" in trajectory
 
-if __name__ == "__main__":
-    test_url_1 = f"{ROCKETCHAT_URL}/home"
-    result1 = check_url_1(f"ACTION: goto('{test_url_1}')")
-    result2 = check_channel_exists("literaturereview")
-    test_url_2 = f"{ROCKETCHAT_URL}/channel/literaturereview"
-    result3 = check_url_2(f"ACTION: goto('{test_url_2}')")
-    result4 = check_channel_exists("benchmarking")
-    test_url_3 = f"{ROCKETCHAT_URL}/channel/benchmarking"
-    result5 = check_url_2(f"ACTION: goto('{test_url_3}')")
-    result6 = check_user_added("literaturereview", "emma_lewis")
-    result7 = check_user_added("literaturereview", "wang_fang")
-    result8 = check_message_sent("literaturereview", 'Welcome to the literature review channel!', "theagentcompany")
-    result9 = check_user_added("benchmarking", "emma_lewis")
-    result10 = check_user_added("benchmarking", "alex_turner")
-    result11 = check_user_added("benchmarking", "david_wong")
-    result12 = check_message_sent("benchmarking", 'This will be for all our benchmarking projects.', "theagentcompany")
+def grade_checkpoint3(trajectory):
+    return f"{ROCKETCHAT_URL}/channel/literaturereview" in trajectory
 
-    # Summing all results to create a score
-    score = (result1 + result2 + result3 + result4 + result5 + 
-             result6 + result7 + result8 + result9 + result10 + 
-             result11 + result12)
-    
-    print("Total score:", score)
+def grade_checkpoint5(trajectory):
+    return f"{ROCKETCHAT_URL}/channel/benchmarking" in trajectory
+
+def grade_checkpoint2():
+    return check_channel_exists("literaturereview")
+
+def grade_checkpoint4():
+    return check_channel_exists("benchmarking")
+
+def grade_checkpoint6():
+    return check_user_added("literaturereview", "emma_lewis")
+
+def grade_checkpoint7():
+    return check_user_added("literaturereview", "wang_fang")
+
+def grade_checkpoint9():
+    return check_user_added("benchmarking", "emma_lewis")
+
+def grade_checkpoint10():
+    return check_user_added("benchmarking", "alex_turner")
+
+def grade_checkpoint11():
+    return check_user_added("benchmarking", "david_wong")
+
+def grade_checkpoint8():
+    return check_message_sent("literaturereview", 'Welcome to the literature review channel!', "theagentcompany")
+
+def grade_checkpoint12():
+    return check_message_sent("benchmarking", 'This will be for all our benchmarking projects.', "theagentcompany")
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints)
+
+    passed1 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed1)))
+
+    passed2 = grade_checkpoint2(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed2)))
+
+    passed3 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed3)))
+
+    passed4 = grade_checkpoint2(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed4)))
+
+    passed5 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed5)))
+
+    passed6 = grade_checkpoint2(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed6)))
+
+    passed7 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed7)))
+
+    passed8 = grade_checkpoint2(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed8)))
+
+    passed9 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed9)))
+
+    passed10 = grade_checkpoint2(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed10)))
+
+    passed11 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed11)))
+
+    passed12 = grade_checkpoint2(trajectory)
+    checkpoints.append(Checkpoint(1, 1 * int(passed12)))
+
+    return result
