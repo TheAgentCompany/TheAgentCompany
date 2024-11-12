f19f602d484bab8c64b0bb41caf70fbc58d6a4e2
veronicium <xyiqing2@illinois.edu>
2024-11-11 22:55:09 -0500

    add task: ds-answer-spreadsheet-questions

---
+++ workspaces/tasks/ds-answer-spreadsheet-questions/checkpoints.md
@@ -0,0 +1,25 @@
+# Checkpoints
+
+## Checkpoint 1 (1 pt)
+
+Confirmed the agent visited the file link on NextCloud
+
+## Checkpoint 2 (1 pt)
+
+Confirmed that agent has conversation history with Li Ming
+
+## Checkpoint 3 (1 pt)
+
+Confirmed question 1 answer ("Male") is mentioned in the conversation with Li Ming
+
+## Checkpoint 4 (1 pt)
+
+Confirmed question 2 answer ("Married") is mentioned in the conversation with Li Ming
+
+## Checkpoint 5 (1 pt)
+
+Confirmed question 3 answer ("French-language workers") is mentioned in the conversation with Li Ming
+
+## Checkpoint 6 (1 pt)
+
+Confirmed the answers are formatted correctly, i.e., separated by lines
---
+++ workspaces/tasks/ds-answer-spreadsheet-questions/evaluator.py
@@ -0,0 +1,48 @@
+import os
+import requests
+from typing import List
+from common import get_rocketchat_personal_chat_history
+from scoring import Result, Checkpoint
+from common import create_rocketchat_client, grader
+
+# Initialize RocketChat client
+rocket = create_rocketchat_client()
+
+@grader
+def grade_checkpoint1(trajectory):
+    return "apps/files/files/1447?dir=/Documents/Data%20Analysis" in trajectory
+
+
+def grade_checkpoints(trajectory="") -> Result:
+    checkpoints: List[Checkpoint] = []
+    result = Result(checkpoints)
+
+    # checkpoint 1
+    passed1 = grade_checkpoint1(trajectory)
+    checkpoints.append(Checkpoint(1, int(passed1)))
+
+    # checkpoint 2
+    chat_history = get_rocketchat_personal_chat_history(rocket, "Li Ming", content_only=True)
+    passed2 = len(chat_history) > 0
+    checkpoints.append(Checkpoint(1, int(passed2)))
+    
+    # checkpoint 3-5
+    msg = "\n".join(chat_history).lower()
+    passed3 = "male" in msg
+    checkpoints.append(Checkpoint(1, int(passed3)))
+    passed4 = "married" in msg
+    checkpoints.append(Checkpoint(1, int(passed4)))
+    passed5 = "french-language workers" in msg
+    checkpoints.append(Checkpoint(1, int(passed5)))
+    
+    # checkpoint 6
+    passed6 = 0
+    for msg in chat_history:
+        segs = msg.lower().split("\n")
+        if len(segs) >= 3:
+            if "male" in segs[0] and "married" in segs[1] and "french-language workers" in segs[2]:
+                passed6 = 1
+
+    checkpoints.append(Checkpoint(1, int(passed6)))
+
+    return result
