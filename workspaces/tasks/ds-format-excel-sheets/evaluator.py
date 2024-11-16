af2138f413b32ff566c56844e64604e9c4e08821
veronicium <xyiqing2@illinois.edu>
2024-11-16 17:49:06 -0500

    refactoring evaluator.py and using get_binary_file_content_nextcloud for file download

--- workspaces/tasks/ds-format-excel-sheets/evaluator.py
+++ workspaces/tasks/ds-format-excel-sheets/evaluator.py
@@ -12,111 +12,78 @@ import openpyxl
 import pandas as pd
 from openpyxl.styles import PatternFill
 
+FILE_PATH = '/workspace/respondent_counts.xlsx'
+INIT_FILE_PATH = '/utils/respondent_counts.xlsx'
+
+
+def load_spreadsheet(file_path, sheet_name, return_type="workbook"):
+    if not os.path.exists(file_path):
+        logging.warning("Excel file not found in the data folder.")
+        return None
+    
+    try:
+        if return_type == "workbook":
+            spreadsheet = openpyxl.load_workbook(file_path)[sheet_name]
+        elif return_type == "dataframe":
+            spreadsheet = pd.read_excel(file_path, sheet_name=sheet_name)
+        return spreadsheet
+    
+    except:
+        logging.warning(f"Cannot read the file: {file_path}.")
+        return None
+
 
 @grader
-def grade_checkpoint1():
-    try:
-        file_url = 'https://ogma.lti.cs.cmu.edu/s/7gGd5AQ5M7WDsL8/download/respondent_counts.xlsx'
-        file_path = '/workspace/respondent_counts.xlsx'
-        init_file_path = '/utils/respondent_counts.xlsx'
-        
-        # Download the file from NextCloud
-        if not download_nextcloud_content(file_url, file_path):
-            logging.warning("Failed to download the Excel file from NextCloud.")
-            return False
-
-        df_init = pd.read_excel(init_file_path, sheet_name="unformatted")
-        try:
-            df = pd.read_excel(file_path, sheet_name="unformatted")
-        except:
-            logging.warning("Cannot read the 'unformatted' sheet.")
-            return False
-        
-        # Check if the 'unformatted' sheet remains unchanged
-        if df_init.equals(df):
-            logging.info("The 'unformatted' sheet remains unchanged.")
-            return True
-        else:
-            logging.warning("The 'unformatted' sheet has been changed.")
-            return False
-    except Exception as e:
-        logging.error(f"Error occurred while checking the 'unformatted' sheet: {e}")
-        return False
+def grade_checkpoint1():    
+    df_init = load_spreadsheet(INIT_FILE_PATH, sheet_name="unformatted", return_type="dataframe")
+    df = load_spreadsheet(FILE_PATH, sheet_name="unformatted", return_type="dataframe")
+    
+    # Check if the 'unformatted' sheet remains unchanged
+    return df is not None and df_init.equals(df)
+    
 
 @grader
 def grade_checkpoint2():
-    try:
-        file_path = '/workspace/respondent_counts.xlsx'
-        if not os.path.exists(file_path):
-            logging.warning("Excel file not found in the data folder.")
-            return False
-        
-        try:
-            df_orig = pd.read_excel(file_path, sheet_name="unformatted")
-        except:
-            logging.warning("Cannot read the 'unformatted' sheet.")
-            return False
+    df_orig = load_spreadsheet(FILE_PATH, sheet_name="unformatted", return_type="dataframe")
+    df = load_spreadsheet(FILE_PATH, sheet_name="formatted", return_type="dataframe")
+    
+    # The content of the 'formatted' sheet should be the same as 'unformatted'
+    return df is not None and df_orig is not None and df_orig.equals(df)
 
-        try:
-            df = pd.read_excel(file_path, sheet_name="formatted")
-        except:
-            logging.warning("Cannot read the 'formatted' sheet.")
-            return False
-        
-        # The content of the 'formatted' sheet should remain unchanged
-        if df_orig.equals(df):
-            logging.info("The content of the 'formatted' sheet remains unchanged.")
-            return True
-        else:
-            logging.warning("The content of the 'formatted' sheet has been changed.")
-            return False
-
-    except Exception as e:
-        logging.error(f"Error occurred while checking the 'formatted' sheet: {e}")
-        return False
 
 @grader
 def grade_checkpoint3():
-    try:
-        file_path = '/workspace/respondent_counts.xlsx'
-        if not os.path.exists(file_path):
-            logging.warning("Excel file not found in the data folder.")
-            return False
-        
-        # Load the workbook
-        wb = openpyxl.load_workbook(file_path)
-        ws_orig = wb['unformatted']
-        ws = wb['formatted']
-        
-        # Access a specific cell
-        cells_to_modify = ["A3", "B3", "B5", "C3", "C4", "D4", "D5", "E5", "F3", "F4", "G4", "G5", "H5", "I3", "I4", "J4", "J5", "K5"]
-        
-        # The background color of cells in 'cells_to_modify' should be #87CEEB
-        # The background color of other cells should remain unchanged
-        for col in "ABCDEFGHIJK":
-            for row in range(1,22):
-                cell_name = f"{col}{row}"
-                
-                cell = ws[cell_name]
-                fill = cell.fill
-                
-                if cell_name in cells_to_modify:
-                    if fill.fill_type == 'solid':
-                        color_hex = fill.fgColor.rgb  # Get the RGB color code
-                        if color_hex != "FF87CEEB":
-                            logging.warning(f"Cell {cell_name} has wrong background color: Expected #87CEEB, obtained {color_hex}.")
-                            return False
-                    else:
-                        logging.warning(f"Cell {cell_name} has wrong background color: Expected #87CEEB, obtained 'None'.")
-                        
-                    continue
-                
-                # orig cell
+    ws_orig = load_spreadsheet(FILE_PATH, sheet_name="unformatted", return_type="workbook")
+    ws = load_spreadsheet(FILE_PATH, sheet_name="formatted", return_type="workbook")
+    if ws is None or ws_orig is None:
+        return False 
+
+    # Access a specific cell
+    cells_to_modify = ["A3", "B3", "B5", "C3", "C4", "D4", "D5", "E5", "F3", "F4", "G4", "G5", "H5", "I3", "I4", "J4", "J5", "K5"]
+    for col in "ABCDEFGHIJK":
+        for row in range(1,22):
+            cell_name = f"{col}{row}"
+            
+            cell = ws[cell_name]
+            fill = cell.fill
+            
+            if cell_name in cells_to_modify:
+                # The background color of cells in 'cells_to_modify' should be #87CEEB
+                if fill.fill_type == 'solid':
+                    color_hex = fill.fgColor.rgb
+                    if color_hex != "FF87CEEB":
+                        logging.warning(f"Cell {cell_name} has wrong background color: Expected #87CEEB, obtained {color_hex}.")
+                        return False
+                else:
+                    logging.warning(f"Cell {cell_name} has wrong background color: Expected #87CEEB, obtained 'None'.")
+
+            else:
+                # The background color of other cells should remain unchanged
                 cell_orig = ws_orig[cell_name]
                 fill_orig = cell_orig.fill
                 
                 if fill.fill_type != fill_orig.fill_type:
-                    logging.warning(f"Cell {cell_name} should not change the background color.")
+                    logging.warning(f"Cell {cell_name} should not change the background fill type.")
                     return False
                 
                 if fill_orig.fill_type == 'solid':
@@ -126,68 +93,47 @@ def grade_checkpoint3():
                     if color_hex != color_hex_orig:
                         logging.warning(f"Cell {cell_name} should not change the background color.")
                         return False
-                
-                
-        logging.info("The required background color changes are applied.")
-        return True
+    
+    logging.info("The required background color changes are correctly applied.")
+    return True
 
-    except Exception as e:
-        logging.error(f"Error occurred while checking the background color: {e}")
-        return False
     
 @grader
 def grade_checkpoint4():
-    try:
-        file_path = '/workspace/respondent_counts.xlsx'
-        if not os.path.exists(file_path):
-            logging.warning("Excel file not found in the data folder.")
-            return False
-        
-        # Load the workbook
-        wb = openpyxl.load_workbook(file_path)
-        ws = wb['formatted']
-        
-        for col in "ABCDEFGHIJK":
-            for row in range(1,22):
-                cell_name = f"{col}{row}"
-                
-                cell = ws[cell_name]
-                if cell.alignment.horizontal != 'center':
-                    logging.warning(f"Cell {cell_name} is not horizontally centered")
-                    return False
-                
-                return True
-                
-        logging.info("All the cells are horizontally centered.")
-        return True
-
-    except Exception as e:
-        logging.error(f"Error occurred while checking the centering of the cells: {e}")
+    ws = load_spreadsheet(FILE_PATH, sheet_name="formatted", return_type="workbook")
+    if ws is None:
         return False
     
+    for col in "ABCDEFGHIJK":
+        for row in range(3,22):
+            cell_name = f"{col}{row}"
+            
+            cell = ws[cell_name]
+            if cell.value and cell.alignment.horizontal != 'center':
+                logging.warning(f"Cell {cell_name} is not horizontally centered (obtained: {cell.alignment.horizontal})")
+                return False
+            
+    logging.info("All the cells are horizontally centered.")
+    return True
     
 
 def grade_checkpoints(trajectory="") -> Result:
     checkpoints: List[Checkpoint] = []
     result = Result(checkpoints)
+    
+    # Download updated file from NextCloud
+    get_binary_file_content_nextcloud("respondent_counts.xlsx", "Documents/Data%20Analysis")
 
     # Checkpoint 1: The 'unformatted' sheet remains unchanged
-    passed_checkpoint1 = grade_checkpoint1()
-    checkpoints.append(Checkpoint(1, int(passed_checkpoint1)))
+    checkpoints.append(Checkpoint(1, int(grade_checkpoint1())))
 
     # Checkpoint 2: The 'formatted' sheet is created and the content is the same as 'unformatted'
-    passed_checkpoint2 = grade_checkpoint2()
-    checkpoints.append(Checkpoint(1, int(passed_checkpoint2)))
+    checkpoints.append(Checkpoint(1, int(grade_checkpoint2())))
 
     # Checkpoint 3: The required background colors are applied
-    passed_checkpoint3 = passed_checkpoint2 and grade_checkpoint3()
-    checkpoints.append(Checkpoint(1, int(passed_checkpoint3)))
+    checkpoints.append(Checkpoint(1, int(grade_checkpoint3())))
     
     # Checkpoint 4: The cells are horizontally centered
-    passed_checkpoint4 = passed_checkpoint2 and grade_checkpoint4()
-    checkpoints.append(Checkpoint(1, int(passed_checkpoint4)))
+    checkpoints.append(Checkpoint(1, int(grade_checkpoint4())))
 
     return result
-
-
-
