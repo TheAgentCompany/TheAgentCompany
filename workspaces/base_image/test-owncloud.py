8b310ca4f6c7f055153ccf8f8a357400a74dc241
Raj Mehta <rajmehta231@gmail.com>
2024-11-21 00:16:03 -0500

    newlines

--- workspaces/base_image/test-owncloud.py
+++ workspaces/base_image/test-owncloud.py
@@ -33,12 +33,12 @@ def test_get_binary_file_content_owncloud():
   
     content = get_binary_file_content_owncloud(filename, dir_name)
     if content:
-        print(f"File content: {content[:100]}...")  # Print first 100 bytes
+        print(f"File content: {content[:100]}...")
     else:
         print("Failed to retrieve file content")
 
 if __name__ == "__main__":
     test_get_owncloud_url_in_file()
     test_download_owncloud_content()
-    #test_check_file_in_owncloud_directory()
-    #test_get_binary_file_content_owncloud()
+    test_check_file_in_owncloud_directory()
+    test_get_binary_file_content_owncloud()
