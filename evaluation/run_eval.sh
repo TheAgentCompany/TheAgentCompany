49272418f5b58cf8e3550351168458616b8679ca
Boxuan Li <liboxuan@connect.hku.hk>
2024-11-23 10:33:09 -0800

    Use host.docker.internal

--- evaluation/run_eval.sh
+++ evaluation/run_eval.sh
@@ -19,7 +19,7 @@ LLM_CONFIG="claude"
 OUTPUTS_PATH="outputs"
 # SERVER_HOSTNAME is the hostname of the server that hosts all the web services,
 # including RocketChat, ownCloud, GitLab, and Plane.
-SERVER_HOSTNAME="localhost"
+SERVER_HOSTNAME="host.docker.internal"
 
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
